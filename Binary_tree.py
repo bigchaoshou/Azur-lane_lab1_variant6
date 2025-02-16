fddf123ef5382f44e984ef20bf2ce0bf7b3fb151
bigchaoshou <155956986+bigchaoshou@users.noreply.github.com>
2025-02-16 23:56:24 +0300

    Add files via upload

--- Binary_tree.py
+++ Binary_tree.py
@@ -1,22 +1,17 @@
-class TreeNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.left = None
-        self.right = None
-
 class BSTDictionary:
     def __init__(self):
         self.root = None
         self._size = 0
-#size
+
+    # size
     def size(self):
         return self._size
-#add
+
+    # add
     def add(self, key, value):
         """在字典中插入键值对"""
         if self.root is None:
-            self.root = TreeNode(key, value)
+            self.root = BSTNode(key, value)
         else:
             self._add_recursive(self.root, key, value)
         self._size += 1
@@ -24,18 +19,18 @@ class BSTDictionary:
     def _add_recursive(self, node, key, value):
         if key < node.key:
             if node.left is None:
-                node.left = TreeNode(key, value)
+                node.left = BSTNode(key, value)
             else:
                 self._add_recursive(node.left, key, value)
         elif key > node.key:
             if node.right is None:
-                node.right = TreeNode(key, value)
+                node.right = BSTNode(key, value)
             else:
                 self._add_recursive(node.right, key, value)
         else:
             node.value = value  # 如果键已存在，更新值
 
-#toList
+    # toList
     def to_list(self):
         """按序遍历返回 (key, value) 列表"""
         result = []
@@ -48,7 +43,7 @@ class BSTDictionary:
             result.append((node.key, node.value))
             self._inorder_traversal(node.right, result)
 
-#map
+    # map
     def map(self, func):
         """对所有值应用函数"""
         self._map_recursive(self.root, func)
@@ -58,7 +53,8 @@ class BSTDictionary:
             node.value = func(node.value)
             self._map_recursive(node.left, func)
             self._map_recursive(node.right, func)
-#reduce
+
+    # reduce
     def reduce(self, func, initial_state):
         """对所有 (key, value) 进行 reduce 计算"""
         state = initial_state
@@ -67,11 +63,11 @@ class BSTDictionary:
     def _reduce_recursive(self, node, func, state):
         if node is not None:
             state = self._reduce_recursive(node.left, func, state)
-            state = func(state, (node.key, node.value))
+            state = func(state, node.value)
             state = self._reduce_recursive(node.right, func, state)
         return state
 
-#search
+    # search
     def search(self, key):
         """查找 key，返回对应的 value"""
         node = self._search_recursive(self.root, key)
@@ -80,47 +76,60 @@ class BSTDictionary:
     def _search_recursive(self, node, key):
         if node is None or node.key == key:
             return node
-        if key < node.key:
+        elif key < node.key:
             return self._search_recursive(node.left, key)
         else:
             return self._search_recursive(node.right, key)
 
-#delete
+    # delete
     def delete(self, key):
         """删除 key 并维护 BST 结构"""
         self.root = self._delete_recursive(self.root, key)
+        self._size -= 1
 
     def _delete_recursive(self, node, key):
         if node is None:
-            return None
+            return node
         if key < node.key:
             node.left = self._delete_recursive(node.left, key)
         elif key > node.key:
             node.right = self._delete_recursive(node.right, key)
         else:
-            # 节点有两个子节点
-            if node.left and node.right:
-                successor = self._find_min(node.right)  # 找右子树最小值
-                node.key, node.value = successor.key, successor.value
-                node.right = self._delete_recursive(node.right, successor.key)
-            else:  # 只有一个子节点或者无子节点
-                node = node.left if node.left else node.right
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            temp = self._find_min(node.right)
+            node.key, node.value = temp.key, temp.value
+            node.right = self._delete_recursive(node.right, temp.key)
         return node
 
     def _find_min(self, node):
-        """找到 BST 最小值（最左侧节点）"""
-        while node.left:
+        while node.left is not None:
             node = node.left
         return node
 
-#update
-    def update(self, key, new_value):
+    # set (update)
+    def set(self, key, new_value):
         """更新 key 的 value"""
         node = self._search_recursive(self.root, key)
         if node:
             node.value = new_value
 
-#tolist 重复了
+    # is a member
+    # is a member
+    def member(self, value):
+        """检查是否有节点的值等于给定值"""
+        return self._member_recursive(self.root, value)
+
+    def _member_recursive(self, node, value):
+        if node is None:
+            return False
+        if node.value == value:
+            return True
+        return self._member_recursive(node.left, value) or self._member_recursive(node.right, value)
+
+    # inorder traversal
     def inorder_traversal(self):
         """中序遍历（升序输出所有键值对）"""
         result = []
@@ -128,46 +137,21 @@ class BSTDictionary:
         return result
 
     def _inorder_recursive(self, node, result):
-        if node:
+        if node is not None:
             self._inorder_recursive(node.left, result)
             result.append((node.key, node.value))
             self._inorder_recursive(node.right, result)
 
-""" 
-    add , size , tolist , map , reduce , search , delete 
-    无Filter, From List, Is a member, Set, update改成Set,iterator, concat
-    
-"""
+class BSTNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
 
-# 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     d = BSTDictionary()
-    d.add(5, "apple")
-    d.add(3, "banana")
-    d.add(7, "cherry")
-    d.add(4, "date")
+    d.add(1,2)
+    print(d.member(2))
+    print(d.size())
 
-    print(d.to_list())  # [(3, 'banana'), (4, 'date'), (5, 'apple'), (7, 'cherry')]
-    print(d.search(4))  # 'date'
-    print(d.search(10))  # None
-
-    d.add(8, "A")
-    d.add(2, "B")
-    d.add(5, "C")
-    d.add(9, "D")
-    d.add(1, "E")
-
-    print(d.search(5))  # 输出: C
-    print(d.search(1))  # 输出: E
-
-    d.update(5, "Z")
-    print(d.search(5))  # 输出: Z
-
-    d.delete(7)
-    print(d.inorder_traversal())  # 输出: [(3, 'B'), (5, 'Z'), (6, 'D'), (8, 'E')]
-
-    d.map(lambda v: v.upper())
-    print(d.to_list())  # [(3, 'BANANA'), (4, 'DATE'), (5, 'APPLE'), (7, 'CHERRY')]
-
-    total_length = d.reduce(lambda acc, pair: acc + len(pair[1]), 0)
-    print(total_length)  # 22
