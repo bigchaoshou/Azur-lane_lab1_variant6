33a7919d4189995e21933b88f1c84eb47570ee33
bigchaoshou <155956986+bigchaoshou@users.noreply.github.com>
2025-02-19 22:26:18 +0300

    Add files via upload

--- Binary_tree.py
+++ Binary_tree.py
@@ -1,3 +1,10 @@
+class BSTNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+
 class BSTDictionary:
     def __init__(self):
         self.root = None
@@ -30,42 +37,6 @@ class BSTDictionary:
         else:
             node.value = value  # 如果 key 已存在，更新 value
 
-    # toList
-    def to_list(self):
-        """按序遍历返回 (key, value) 列表"""
-        result = []
-        self._inorder_traversal(self.root, result)
-        return result
-
-    def _inorder_traversal(self, node, result):
-        if node is not None:
-            self._inorder_traversal(node.left, result)
-            result.append((node.key, node.value))
-            self._inorder_traversal(node.right, result)
-
-    # map
-    def map(self, func):
-        """对所有值应用函数"""
-        self._map_recursive(self.root, func)
-
-    def _map_recursive(self, node, func):
-        if node is not None:
-            node.value = func(node.value)
-            self._map_recursive(node.left, func)
-            self._map_recursive(node.right, func)
-
-    # reduce
-    def reduce(self, func, initial_state):
-        """对所有 (key, value) 进行 reduce 计算"""
-        return self._reduce_recursive(self.root, func, initial_state)
-
-    def _reduce_recursive(self, node, func, state):
-        if node is not None:
-            state = self._reduce_recursive(node.left, func, state)
-            state = func(state, node.value)
-            state = self._reduce_recursive(node.right, func, state)
-        return state
-
     # search
     def search(self, key):
         """查找 key，返回对应的 value"""
@@ -80,8 +51,16 @@ class BSTDictionary:
         else:
             return self._search_recursive(node.right, key)
 
-    # delete
-    def delete(self, key):
+
+    # set
+    def set(self, key, new_value):
+        """更新 key 的 value"""
+        node = self._search_recursive(self.root, key)
+        if node:
+            node.value = new_value
+
+    # remove
+    def remove(self, key):
         """删除 key 并维护 BST 结构"""
         self.root = self._delete_recursive(self.root, key)
         self._size -= 1
@@ -103,19 +82,7 @@ class BSTDictionary:
             node.right = self._delete_recursive(node.right, temp.key)
         return node
 
-    def _find_min(self, node):
-        while node.left is not None:
-            node = node.left
-        return node
-
-    # set (update)
-    def set(self, key, new_value):
-        """更新 key 的 value"""
-        node = self._search_recursive(self.root, key)
-        if node:
-            node.value = new_value
-
-    # is a member
+    # member
     def member(self, value):
         """检查是否有节点的值等于给定值"""
         return self._member_recursive(self.root, value)
@@ -127,78 +94,163 @@ class BSTDictionary:
             return True
         return self._member_recursive(node.left, value) or self._member_recursive(node.right, value)
 
-    # inorder traversal
-    def inorder_traversal(self):
-        """中序遍历（升序输出所有键值对）"""
+    # reverse
+    def reverse(self):
+        """反转字典的顺序"""
         result = []
-        self._inorder_recursive(self.root, result)
+        self._inorder_traversal(self.root, result)
+        return result[::-1]  # 反转列表
+
+    # from_list
+    @classmethod
+    def from_list(cls, lst):
+        """根据给定的 (key, value) 元组列表，创建一个 BSTDictionary"""
+        bst_dict = cls()
+        for key, value in lst:
+            bst_dict.add(key, value)
+        return bst_dict
+
+    # to_list
+    def to_list(self):
+        """按序遍历返回 (key, value) 列表"""
+        result = []
+        self._inorder_traversal(self.root, result)
         return result
 
-    def _inorder_recursive(self, node, result):
+    def _inorder_traversal(self, node, result):
         if node is not None:
-            self._inorder_recursive(node.left, result)
+            self._inorder_traversal(node.left, result)
             result.append((node.key, node.value))
-            self._inorder_recursive(node.right, result)
+            self._inorder_traversal(node.right, result)
 
-    # concat (合并 BST，但不覆盖已有的 key)
+    # filter
+    def filter(self, predicate):
+        """根据给定的条件过滤元素"""
+        result = []
+        self._filter_recursive(self.root, predicate, result)
+        return result
+
+    def _filter_recursive(self, node, predicate, result):
+        if node:
+            if predicate(node.key, node.value):
+                result.append((node.key, node.value))
+            self._filter_recursive(node.left, predicate, result)
+            self._filter_recursive(node.right, predicate, result)
+
+    # map
+    def map(self, func):
+        """对字典中的每个元素应用指定的函数"""
+        result = []
+        self._map_recursive(self.root, func, result)
+        return result
+
+    def _map_recursive(self, node, func, result):
+        if node:
+            result.append(func(node.key, node.value))
+            self._map_recursive(node.left, func, result)
+            self._map_recursive(node.right, func, result)
+
+    # reduce
+    def reduce(self, func, initial_value):
+        """通过指定的函数对字典中的元素进行归约"""
+        return self._reduce_recursive(self.root, func, initial_value)
+
+    def _reduce_recursive(self, node, func, value):
+        if node is None:
+            return value
+        value = self._reduce_recursive(node.left, func, value)
+        value = func(value, node.key, node.value)
+        value = self._reduce_recursive(node.right, func, value)
+        return value
+
+    # iterator
+    def __iter__(self):
+        """返回一个迭代器，支持 `for key, value in d:`"""
+        self._iter_stack = []
+        self._push_left(self.root)
+        return self
+
+    def __next__(self):
+        """返回下一个 (key, value)"""
+        if not self._iter_stack:
+            raise StopIteration
+        node = self._iter_stack.pop()
+        self._push_left(node.right)
+        return node.key, node.value
+
+    def _push_left(self, node):
+        """辅助方法：将左子树压入栈"""
+        while node:
+            self._iter_stack.append(node)
+            node = node.left
+
+    # empty
+    @classmethod
+    def empty(cls):
+        """返回一个空的 BSTDictionary"""
+        return cls()
+
+    # concat
     def concat(self, other):
         """将另一个 BSTDictionary 合并到当前字典中，保留已有 key"""
+        if not isinstance(other, BSTDictionary) or other.root is None:
+            return
         def add_if_absent(node):
             if node is not None:
-                if self.search(node.key) is None:  # 只有 key 不存在才添加
+                if self.search(node.key) is None:
                     self.add(node.key, node.value)
                 add_if_absent(node.left)
                 add_if_absent(node.right)
-
         add_if_absent(other.root)
 
-    # iterator
-    def iter_from(self, start_key):
-        """从 `start_key` 开始进行中序遍历"""
-        stack = []
-        node = self.root
-        found = False
+# 测试代码
+if __name__ == "__main__":
+    lst = BSTDictionary()
+    lst.add(1, "A")
+    lst.add(2, "B")
+    lst.add(3, "C")
 
-        while stack or node:
-            while node:
-                stack.append(node)
-                node = node.left
+    # 删除元素
+    print("Before remove:")
+    print(lst.to_list())
+    lst.remove(2)
+    print("After remove:")
+    print(lst.to_list())
 
-            node = stack.pop()
+    # 过滤
+    print("\nFiltered (even keys):")
+    even = lst.filter(lambda k, v: k % 2 == 0)
+    print(even)
 
-            if node.key >= start_key:
-                found = True
+    # 映射
+    print("\nMapped (increment keys by 1):")
+    incremented = lst.map(lambda k, v: (k + 1, v))
+    print(incremented)
 
-            if found:
-                yield (node.key, node.value)
+    # 归约
+    print("\nReduced (sum of keys):")
+    total = lst.reduce(lambda acc, k, v: acc + k, 0)
+    print(total)
 
-            node = node.right
+    # 反转
+    print("\nReversed List:")
+    reversed_lst = lst.reverse()
+    print(reversed_lst)
 
+    # 转换为列表
+    print("\nList form:")
+    lst_list = lst.to_list()
+    print(lst_list)
 
-class BSTNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.left = None
-        self.right = None
+    # 创建空字典
+    empty_dict = BSTDictionary.empty()
+    print("\nEmpty dictionary:")
+    print(empty_dict.to_list())
 
-
-# 示例使用
-if __name__ == '__main__':
-    d1 = BSTDictionary()
-    d1.add(1, 2)
-    d1.add(3, 4)
-
-    d2 = BSTDictionary()
-    d2.add(2, 3)
-    d2.add(4, 5)
-    d2.add(3, 5)
-    # 使用 concat 合并字典
-    d1.concat(d2)
-
-    # 使用 iterator 迭代
-    print("=== 从 key = 3 开始迭代 ===")
-    for key, value in d1.iter_from(3):
-        print(f"Key: {key}, Value: {value}")
-
-    print(f"合并后的字典大小: {d1.size()}")
+    # 合并字典
+    print("\nConcatenate with another dictionary:")
+    lst2 = BSTDictionary()
+    lst2.add(4, "D")
+    lst2.add(5, "E")
+    lst.concat(lst2)
+    print(lst.to_list())
