a01dc13c8fa2e5f59bd727d78346d5b7573d92a0
Futano <2395604522@qq.com>
2025-04-06 12:36:56 +0300

    注解，测试集修改

--- binary_tree.py
+++ binary_tree.py
@@ -1,210 +1,127 @@
-from typing import Any, Optional, List, Tuple, Callable, TypeVar, Iterator
+from __future__ import annotations
+from typing import (
+    Any, Optional, Callable, List, Tuple,
+    Iterator, Iterable, Generic, TypeVar,
+    Union, Mapping
+)
+from typing_extensions import Protocol
 
 
-K = TypeVar('K')
-V = TypeVar('V')
-T = TypeVar('T')
-
-class BSTNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.left = None
-        self.right = None
+class SupportsRichComparison(Protocol):
+    def __lt__(self, other: Any) -> bool: ...
+    def __le__(self, other: Any) -> bool: ...
+    def __gt__(self, other: Any) -> bool: ...
+    def __ge__(self, other: Any) -> bool: ...
 
 
-class BSTDictionary:
-    def __init__(self):
-        self.root = None
-        self._size = 0
+KT = TypeVar("KT", bound=SupportsRichComparison)
+VT = TypeVar("VT")
+AccT = TypeVar("AccT")
 
-    def size(self):
-        return self._size
 
-    def add(self, key, value):
-        if self.root is None:
-            self.root = BSTNode(key, value)
-            self._size += 1
-        else:
-            if self._add_recursive(self.root, key, value):
-                self._size += 1
+class DictDictionary(Generic[KT, VT]):
+    def __init__(self) -> None:
+        self._dict: dict[KT, VT] = {}
 
-    def _add_recursive(self, node, key, value):
-        if key < node.key:
-            if node.left is None:
-                node.left = BSTNode(key, value)
-                return True
-            else:
-                return self._add_recursive(node.left, key, value)
-        elif key > node.key:
-            if node.right is None:
-                node.right = BSTNode(key, value)
-                return True
-            else:
-                return self._add_recursive(node.right, key, value)
-        else:
-            node.value = value
-            return False
+    def size(self) -> int:
+        return len(self._dict)
 
-    def _find_min(self, node):
-        while node.left is not None:
-            node = node.left
-        return node
+    def add(self, key: KT, value: VT) -> None:
+        """Add or update key-value pair"""
+        self._dict[key] = value
 
-    def search(self, key):
-        node = self._search_recursive(self.root, key)
-        return node.value if node else None
+    def search(self, key: KT) -> Optional[VT]:
+        """Search value by key"""
+        return self._dict.get(key, None)
 
-    def _search_recursive(self, node, key):
-        if node is None or node.key == key:
-            return node
-        elif key < node.key:
-            return self._search_recursive(node.left, key)
-        else:
-            return self._search_recursive(node.right, key)
+    def set(self, key: KT, new_value: VT) -> None:
+        """Update existing key's value"""
+        if key in self._dict:
+            self._dict[key] = new_value
 
-    def set(self, key, new_value):
-        node = self._search_recursive(self.root, key)
-        if node:
-            node.value = new_value
+    def remove(self, key: KT) -> None:
+        """Remove key from dictionary"""
+        if key in self._dict:
+            del self._dict[key]
 
-    def remove(self, key):
-        if self.search(key) is not None:
-            self.root = self._delete_recursive(self.root, key)
-            self._size -= 1
+    def member(self, value: VT) -> bool:
+        """Check if value exists"""
+        return value in self._dict.values()
 
-    def _delete_recursive(self, node, key):
-        if node is None:
-            return node
-        if key < node.key:
-            node.left = self._delete_recursive(node.left, key)
-        elif key > node.key:
-            node.right = self._delete_recursive(node.right, key)
-        else:
-            if node.left is None:
-                return node.right
-            elif node.right is None:
-                return node.left
-            temp = self._find_min(node.right)
-            node.key, node.value = temp.key, temp.value
-            node.right = self._delete_recursive(node.right, temp.key)
-        return node
-
-    def member(self, value):
-        return self._member_recursive(self.root, value)
-
-    def _member_recursive(self, node, value):
-        if node is None:
-            return False
-        if node.value == value:
-            return True
-        return (self._member_recursive(node.left, value) or
-                self._member_recursive(node.right, value))
-
-    def reverse(self):
-        result = []
-        self._inorder_traversal(self.root, result)
-        return result[::-1]
+    def reverse(self) -> List[Tuple[KT, VT]]:
+        """Get reversed sorted entries"""
+        return sorted(self._dict.items(), key=lambda x: x[0], reverse=True)
 
     @classmethod
-    def from_list(cls, lst):
-        bst_dict = cls()
+    def from_list(cls, lst: Iterable[Tuple[KT, VT]]) -> DictDictionary[KT, VT]:
+        """Build from iterable of (key, value) pairs"""
+        new_dict = cls()
         for key, value in lst:
-            bst_dict.add(key, value)
-        return bst_dict
+            new_dict.add(key, value)
+        return new_dict
 
-    def to_list(self):
-        result = []
-        self._inorder_traversal(self.root, result)
-        return result
+    def to_list(self) -> List[Tuple[KT, VT]]:
+        """Convert to sorted list"""
+        return sorted(self._dict.items(), key=lambda x: x[0])
 
-    def _inorder_traversal(self, node, result):
-        if node is not None:
-            self._inorder_traversal(node.left, result)
-            result.append((node.key, node.value))
-            self._inorder_traversal(node.right, result)
+    def filter(
+            self,
+            predicate: Callable[[KT, VT], bool]
+    ) -> List[Tuple[KT, VT]]:
+        """Filter entries by predicate"""
+        return sorted(
+            [(k, v) for k, v in self._dict.items() if predicate(k, v)],
+            key=lambda x: x[0]
+        )
 
-    def filter(self, predicate):
-        result = []
-        self._filter_recursive(self.root, predicate, result)
-        return sorted(result, key=lambda x: x[0])
+    def map(
+            self,
+            func: Callable[[KT, VT], Tuple[Any, Any]]
+    ) -> List[Tuple[Any, Any]]:
+        """Apply function to each entry"""
+        return [
+            func(k, v)
+            for k, v in sorted(
+                self._dict.items(),
+                key=lambda x: x[0]
+            )
+        ]
 
-    def _filter_recursive(self, node, predicate, result):
-        if node:
-            if predicate(node.key, node.value):
-                result.append((node.key, node.value))
-            self._filter_recursive(node.left, predicate, result)
-            self._filter_recursive(node.right, predicate, result)
+    def reduce(
+            self,
+            func: Callable[[AccT, KT, VT], AccT],
+            initial_value: AccT
+    ) -> AccT:
+        """Fold entries with initial value"""
+        acc = initial_value
+        for k, v in sorted(self._dict.items(), key=lambda x: x[0]):
+            acc = func(acc, k, v)
+        return acc
 
-    def map(self, func):
-        result = []
-        self._map_recursive(self.root, func, result)
-        return BSTDictionary.from_list(result).to_list()
-
-    def _map_recursive(self, node, func, result):
-        if node:
-            result.append(func(node.key, node.value))
-            self._map_recursive(node.left, func, result)
-            self._map_recursive(node.right, func, result)
-
-    def reduce(self, func, initial_value):
-        return self._reduce_recursive(self.root, func, initial_value)
-
-    def _reduce_recursive(self, node, func, value):
-        if node is None:
-            return value
-        value = self._reduce_recursive(node.left, func, value)
-        value = func(value, node.key, node.value)
-        value = self._reduce_recursive(node.right, func, value)
-        return value
-
-    def __iter__(self):
-        self._iter_stack = []
-        self._push_left(self.root)
-        return self
-
-    def __next__(self):
-        if not self._iter_stack:
-            raise StopIteration
-        node = self._iter_stack.pop()
-        self._push_left(node.right)
-        return node.key, node.value
-
-    def _push_left(self, node):
-        while node:
-            self._iter_stack.append(node)
-            node = node.left
+    def __iter__(self) -> Iterator[Tuple[KT, VT]]:
+        """Sorted iterator"""
+        return iter(sorted(self._dict.items(), key=lambda x: x[0]))
 
     @staticmethod
-    def empty():
-        return BSTDictionary()
+    def empty() -> DictDictionary[Any, Any]:
+        """Create empty dictionary"""
+        return DictDictionary()
 
-    # def concat(self, other):
-    #     if not isinstance(other, BSTDictionary) or other.root is None:
-    #         return self
-    #     new = BSTDictionary()
-    #
-    #     def copy_tree(node):
-    #         if node is not None:
-    #             new.add(node.key, node.value)
-    #             copy_tree(node.left)
-    #             copy_tree(node.right)
-    #     copy_tree(self.root)
-    #
-    #     def add_other_tree(node):
-    #         if node is not None:
-    #             new.add(node.key, node.value)
-    #             add_other_tree(node.left)
-    #             add_other_tree(node.right)
-    #     add_other_tree(other.root)
-    #     return new
-def concat(self, other):
-    if not isinstance(other, BSTDictionary) or other.root is None:
-        return self
-    new = BSTDictionary()
-    def copy_tree(node):
-        if node is not None:
-            new.add(node.key, node.value)
-        copy_tree(node.left)
-        copy_tree(node.right)
-        copy_tree(self.root)
+    def concat(
+            self,
+            other: Union[DictDictionary[KT, VT], Mapping[KT, VT]]
+    ) -> None:
+        """Merge another dictionary into this one (in-place)"""
+        other_dict = (
+            dict(other.items())
+            if isinstance(other, Mapping)
+            else other._dict
+        )
+        self._dict.update(other_dict)
+
+    def items(self) -> Iterable[Tuple[KT, VT]]:
+        """Get sorted items view"""
+        return sorted(
+            self._dict.items(),
+            key=lambda x: x[0]
+        )
